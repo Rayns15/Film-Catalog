11ef95b36fdf4dd5710d528f8fdb88faa5cbda6c
Rayns15 <panaaitemanuel@gmail.com>
2025-11-15 15:46:51 +0200

    Added multiple updates

--- Arch/movies/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Movie, Cinema, Showtime, Profile
-
-
-admin.site.register(Movie)
-admin.site.register(Cinema)
-admin.site.register(Showtime)
-admin.site.register(Profile)
